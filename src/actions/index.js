f2e7caf041c7c045f786a9f57dba06c6783680f3
Ajay Kumar <kumarmishraajay45@gmail.com>
2023-03-21 13:40:26 +0530

    Adding actions

---
+++ src/actions/index.js
@@ -0,0 +1,14 @@
+/*Actions is basically a javascript object.using this object we can express intent to modify the state.
+type property is used to tell redux what action should be taken*/
+
+
+{
+    type:'INCREASE_COUNT'
+    /*YOU CAN SEND OTHER DATA WITH IT TOO
+    movies:[]
+    user:[]
+    */
+}
+{
+    type:'DECREASE_COUNT'
+}
