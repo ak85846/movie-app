a08e36ebc9310f2638cd8e1fdf0f4716f50dc246
Ajay Kumar <kumarmishraajay45@gmail.com>
2023-04-26 02:47:08 +0530

    Creating Store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {createStore} from 'redux';
+
 import './index.css';
 import App from './components/App';
+import movies from './reducers';
 
+
+const store = createStore(movies);
+//the output will be a object
+console.log('store',store);
+//the output will be the empty array (because of default argument as empty array in reucers)
+console.log('STATE',store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
