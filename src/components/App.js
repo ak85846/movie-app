e5c017bbf0cd739b86376550e6a1cfede0732c9a
Ajay Kumar <kumarmishraajay45@gmail.com>
2023-05-02 15:35:02 +0530

    subscribe to the cart

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,31 @@ import React from 'react';
 import {data} from '../data'; 
 import Navbar from './Navbar';
 import MovieCard from './MovieCard';
+import { type } from '@testing-library/user-event/dist/type';
 
-function App() { 
+class App extends React.Component { 
+  componentDidMount(){
+    const {store}=this.props
+    //after dispathing subsribe callbak will be called then other thing
+    store.subscribe(()=>{
+      console.log('UPDATED');
+      //we should not use foreupdate because it is used for forcefully update app component
+      this.forceUpdate();
+    });
+    //make api call to get the movie
+    //dispatch the action
+    //but currently we are not making an api call so let's dispatch
+    store.dispatch({
+      type: 'ADD_MOVIES',
+      movies:data
+    });
+
+    console.log('STATE',this.props.store.getState());
+
+  }
+  render(){
+  const movies=this.props.store.getState();
+  consolelog('Render')
   return (
     <div className="App">
     <Navbar />
@@ -13,13 +36,14 @@ function App() {
        <div className="tab">Favorites</div> 
       </div>
       <div className="list">
-        {data.map((movie,index) => (
+        {movies.map((movie,index) => (
           <MovieCard movie={movie} key={`movies-${index}`} />
         ))}
       </div>
     </div>
     </div>
   );
+}
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import movies from './reducers';
 const store = createStore(movies);
 //the output will be a object
 console.log('store',store);
+/*
 //the output will be the empty array (because of default argument as empty array in reucers)
 console.log(' BEFORE STATE',store.getState());
 //dispatch func send actions to reducers
@@ -19,10 +20,12 @@ store.dispatch({
 });
 //the output will be array of object with name superman
 console.log('AFTER STATE',store.getState());
+*/
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* after giving apps as store now app can get movies from here */}
+    <App store={store}/>
   </React.StrictMode>
 );
 
